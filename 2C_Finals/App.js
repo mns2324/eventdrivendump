8c331610e7caa4781fb3581505d7a19130c7f95c
mns2324 <mnsbartolata@addu.edu.ph>
2026-04-26 13:18:29 +0800

    initial

---
+++ 2C_Finals/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [board, setBoard] = useState(['Unselected', 'Unselected', 'Unselected', 'Unselected', 'Unselected', 'Unselected', 'Unselected', 'Unselected', 'Unselected', 'Unselected']);
+
+  function handleClick(pos) {
+    let newBoard = board.slice();
+    if(board[pos] == "Unselected")
+    {
+      newBoard[pos] = "Selected";
+    } else {
+      newBoard[pos] = "Unselected"
+    }
+    setBoard(newBoard);
+  }
+
+  return (
+    <div className="App">
+      <h1>E-Commerce Store</h1>
+      <p>Select product/s before proceeding to checkout.</p>
+      <table>
+        <tr>
+          <td onClick={() => handleClick(0)}>
+            {board[0]}
+          </td>
+          <td onClick={() => handleClick(1)}>
+            {board[1]}
+          </td>
+        </tr>
+        <tr>
+          <td onClick={() => handleClick(2)}>
+            {board[2]}
+          </td>
+          <td onClick={() => handleClick(3)}>
+            {board[3]}
+          </td>
+        </tr>
+        <tr>
+          <td onClick={() => handleClick(4)}>
+          {board[4]}
+          </td>
+          <td onClick={() => handleClick(5)}>
+          {board[5]}
+          </td>
+        </tr>
+        <tr>
+          <td onClick={() => handleClick(6)}>
+          {board[6]}
+          </td>
+          <td onClick={() => handleClick(7)}>
+          {board[7]}
+          </td>
+        </tr>
+        <tr>
+          <td onClick={() => handleClick(8)}>
+          {board[8]}
+          </td>
+          <td onClick={() => handleClick(9)}>
+          {board[9]}
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
